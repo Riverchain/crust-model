9b32d6a5cb0621958933be160b0bcffc16bfd0fe
Mostafa <ramezani.m@ut.ac.ir>
2020-03-16 13:45:57 +0330

    getting ready to add layout

---
+++ watum.js
@@ -0,0 +1,9 @@
+Mostafa = Array.from("Mostafa");
+console.log("Mostafa = " + Mostafa.reverse());
+console.log("Mostafa = " + Mostafa);
+let i = 0;
+Mostafa.forEach(function use_array_as_loop() {
+
+    console.log(i++);
+
+});
