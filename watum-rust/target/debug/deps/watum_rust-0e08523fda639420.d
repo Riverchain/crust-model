a3cb39302f3fc1e41ee601289eca390927fc5946
Mostafa <ramezani.m@ut.ac.ir>
2020-04-01 19:34:45 +0430

    Two separated folders for old matlab and brand new rust watum created

---
+++ watum-rust/target/debug/deps/watum_rust-0e08523fda639420.d
@@ -0,0 +1,5 @@
+/home/mostafa/Documents/local-repositories/rust-wqm/watum-rust/target/debug/deps/watum_rust-0e08523fda639420: src/main.rs
+
+/home/mostafa/Documents/local-repositories/rust-wqm/watum-rust/target/debug/deps/watum_rust-0e08523fda639420.d: src/main.rs
+
+src/main.rs:
---
+++ watum-rust/target/debug/watum-rust.d
@@ -0,0 +1,1 @@
+/home/mostafa/Documents/local-repositories/rust-wqm/watum-rust/target/debug/watum-rust: /home/mostafa/Documents/local-repositories/rust-wqm/watum-rust/src/main.rs
