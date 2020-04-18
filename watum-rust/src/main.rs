42d3803b58c0515ea1cb2113a1902521eb874d46
Mostafa <ramezani.m@ut.ac.ir>
2020-04-18 18:04:19 +0430

    main function now is able to read a txt file into an array.

--- watum-rust/src/main.rs
+++ watum-rust/src/main.rs
@@ -1,8 +1,21 @@
-use std::env;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::io;
+use std::fs;
 
+fn main() -> io::Result<()> {
+    let mut args = std::env::args();
+    args.next();
+    for arg in args {
+        let lines = file_to_vec(arg)?;
+        println!("{:?}", lines);
+    }
 
+    Ok(())
+}
 
- fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+fn file_to_vec(filename: String) -> io::Result<Vec<String>> {
+    let file_in = fs::File::open(filename)?;
+    let file_reader = BufReader::new(file_in);
+    Ok(file_reader.lines().filter_map(io::Result::ok).collect())
 }
