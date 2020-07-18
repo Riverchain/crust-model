db74c9a287bba0229a93200fe4981cf77c7bfcbf
Mostafa <darth.mostafa@gmail.com>
2020-07-18 13:25:07 +0300

    A test struct added.

--- crust/src/main.rs
+++ crust/src/main.rs
@@ -1,4 +1,24 @@
 fn main() {
+    let ldc = ChooseLDC::Fischer;
+    
 }
 
 
+enum ChooseLDC {
+    Fischer,
+    Qiasi,
+    Noori,
+    Fucker,
+}
+
+struct project_specification {
+    project_name: String,
+    project_author: String,
+    project_owner: String,
+    project_contractor: String,
+    project_date: String,
+    project_location: String,
+    project_email: String,
+    project_website: String,
+    project_status: bool,
+}
